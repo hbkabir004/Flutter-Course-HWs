332ba62b9e2dc6d36e4f96a08d7ca6fa4811ddf9
ABIR <hbkabir004@gmail.com>
2021-10-14 03:11:09 +0600

    HW-Day-11-Tuesday

--- Profile_Page/lib/profilePage.dart
+++ Profile_Page/lib/profilePage.dart
@@ -24,7 +24,7 @@ class _ProfilePageState extends State<ProfilePage> {
             style: TextStyle(
               fontSize: 25,
               fontWeight: FontWeight.w800,
-              color: Colors.grey.shade200,
+              color: Colors.white,
               fontStyle: FontStyle.italic,
             ),
           ),
@@ -50,7 +50,7 @@ class _ProfilePageState extends State<ProfilePage> {
                       style: BorderStyle.solid,
                     ),
                     borderRadius: BorderRadius.circular(15),
-                    color: Colors.green.shade400,
+                    color: Colors.transparent,
                   ),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -66,31 +66,144 @@ class _ProfilePageState extends State<ProfilePage> {
               ),
             ),
           ]),
-      extendBodyBehindAppBar: true,
-      body: Container(
-          height: 500,
-          width: 500,
-          decoration: BoxDecoration(
-            border: Border.all(
-              color: Colors.black,
-              width: 5,
-              style: BorderStyle.solid,
-            ),
-            borderRadius: BorderRadius.circular(25),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("ABIR's Profile Page"),
-              SizedBox(
-                width: 10,
-              ),
-              Text("HW-DAY-08 (Sunday)"),
-              SizedBox(
-                width: 5,
-              ),
-            ],
-          )),
+      // extendBodyBehindAppBar: true,
+      backgroundColor: Color.fromRGBO(246, 246, 246, .6),
+      body: Align(
+        alignment: Alignment.center,
+        child: Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                //Profile Image And Name Container
+
+                Container(
+                  height: MediaQuery.of(context).size.height / 6,
+                  width: MediaQuery.of(context).size.width,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                        height: MediaQuery.of(context).size.height / 6,
+                        width: MediaQuery.of(context).size.width / 3.5,
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage("images/me.png"),
+                              fit: BoxFit.fill,
+                            ),
+                            shape: BoxShape.rectangle,
+                            borderRadius: BorderRadius.circular(10),
+                            border: Border.all(
+                              color: Color.fromRGBO(34, 36, 45, 1),
+                              width: 3,
+                              style: BorderStyle.solid,
+                            )),
+                      ),
+
+                      //profile name and address column
+                      Container(
+                        height: MediaQuery.of(context).size.height / 6,
+                        width: MediaQuery.of(context).size.width / 1.7,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(right: 77),
+                              child: Text(
+                                'Md. Hasanul Banna Khan Abir',
+                                style: TextStyle(
+                                  fontStyle: FontStyle.normal,
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.w700,
+                                  color: Color.fromRGBO(249, 248, 240, 1),
+                                ),
+                                textAlign: TextAlign.start,
+                              ),
+                            ),
+                            Text(
+                              'North Western University, Khulna.',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600,
+                                fontStyle: FontStyle.normal,
+                                color: Color.fromRGBO(249, 248, 240, 1),
+                              ),
+                              textAlign: TextAlign.start,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                //Bio Container
+
+                Container(
+                  height: MediaQuery.of(context).size.height / 2.5,
+                  width: MediaQuery.of(context).size.width,
+                  margin: EdgeInsets.only(
+                    bottom: MediaQuery.of(context).size.height / 5,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      //Text and icon container
+                      Container(
+                        height: MediaQuery.of(context).size.height / 8,
+                        width: MediaQuery.of(context).size.width / 7,
+                        margin: EdgeInsets.only(
+                          right: MediaQuery.of(context).size.width / 1.3,
+                          top: MediaQuery.of(context).size.height / 20,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "Bio",
+                              style: TextStyle(
+                                fontStyle: FontStyle.normal,
+                                fontWeight: FontWeight.w500,
+                                fontSize: 20,
+                                color: Color.fromRGBO(248, 249, 250, .5),
+                              ),
+                            ),
+                            Icon(
+                              Icons.create_rounded,
+                              color: Color.fromRGBO(248, 249, 250, .5),
+                              size: 18,
+                            ),
+                          ],
+                        ),
+                      ),
+
+                      //Text Container
+
+                      Container(
+                        height: MediaQuery.of(context).size.height / 4.6,
+                        width: MediaQuery.of(context).size.width,
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 15,
+                        ),
+                        child: Text(
+                          '''Md. Hasanul Banna Khan Abir, also known as Aungstrome Abir. Studying BSc. in CSE in the North Western University. Currently living in 25/b, Hazi Mohsin Road, Khulna, Bangladesh.
+                          ''',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                            fontStyle: FontStyle.normal,
+                            color: Color.fromRGBO(249, 247, 245, .5),
+                          ),
+                          textAlign: TextAlign.start,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            )),
+      ),
     );
   }
 }
